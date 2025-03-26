3a09e015a75d914526c56278f34a828e850fef3a
daved <daved@codemodus.com>
2025-03-25 17:26:14 -0700

    Remove fastclick

--- themes/custom-cleanwhite/static/js/footer_c.js
+++
@@ -1,4 +0,0 @@
-    loadAsync("https://cdn.jsdelivr.net/npm/fastclick@1.0.6/lib/fastclick.min.js", function(){
-        var $nav = document.querySelector("nav");
-        if($nav) FastClick.attach($nav);
-    })
